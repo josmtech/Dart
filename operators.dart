8bc63610cf4555631b388941b632c6e394d84ee4
Joseph Mumo <josephmmbetu@gmail.com>
2024-05-16 15:28:53 +0300

    artithmetic operators

---
+++ operators.dart
@@ -0,0 +1,20 @@
+void main() {
+    //declaration of variables and initialization
+    int num1 = 23;
+    int num2 = 65;
+
+    //mathematical operation
+    int sum = num1-num2; //addition
+    int difference = num2-num1; //subtration
+    int product = num1*num2; //multiplication
+    double quotient = num2/num1; //division 
+    int intdiv = num2~/num1; //mudulo division
+    int rem = num2%num1; //remainder
+
+    print(sum);
+    print(difference);
+    print(product);
+    print(quotient);
+    print(intdiv);
+    print(rem);
+}
