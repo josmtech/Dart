25771d1a60811791a9e0ed7ee490baa436c9530a
Joseph Mumo <josephmmbetu@gmail.com>
2024-05-16 15:42:10 +0300

    more on functions

--- functions.dart
+++ functions.dart
@@ -3,6 +3,12 @@ void printName() {
   print("My name is $name");
 }
 
+void add(int num1, int num2) {
+  int sum = num1 + num2;
+  print("The sum of the numbers is $sum");
+}
+
 void main() {
   printName();
+  add(20, 15);
 }
