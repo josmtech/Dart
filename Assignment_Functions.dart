a74ac8d5c57e4400092636d024904d12cce72735
Joseph Mumo <josephmmbetu@gmail.com>
2024-05-22 13:13:27 +0300

    functions assignment done

---
+++ Assignment_Functions.dart
@@ -0,0 +1,43 @@
+num addTwo(num firstNumber, num secondNumber) {
+  num sum = firstNumber + secondNumber;
+  return sum;
+}
+
+num subtractTwo(first, second) {
+  num diff = first - second;
+  return diff;
+}
+
+num multiplyTwo(first, second) {
+  num product = first * second;
+  return product;
+}
+
+num divideTwo(first, second) {
+  num quotient = first / second;
+  return quotient;
+}
+
+int stringLength(String) {
+  int length = String.length(String);
+  return length;
+}
+
+String getFirstElement(List) {
+  var first = getFirstElement(List);
+  return first;
+}
+
+void main() {
+  print(addTwo(23.7, 423));
+
+  print(subtractTwo(34, 76.6));
+
+  print(multiplyTwo(20, 3.6));
+
+  print(divideTwo(4, 5));
+
+  print(stringLength('Myname'));
+
+  print(getFirstElement(['Jane', 'Josphine', 'Joan']));
+}
